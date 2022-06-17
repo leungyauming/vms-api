389ed4ce03b97c59dd0926d0d8d4d52b3285c8ec
Leung Yau Ming <leungyauming2005@gmail.com>
2022-06-17 22:13:22 +0800

    setup winston for logging

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import express from 'express';
-
-const app = express();
+import winston from 'winston';
+import logform from 'logform';
 
 const prod = process.env.NODE_ENV === 'production';
 
+winston.configure({
+  format: logform.format.combine(
+    logform.format.colorize({ level: true }),
+    logform.format.simple(),
+  ),
+  transports: [
+    new winston.transports.Console({ level: prod ? 'info' : 'debug' }),
+  ],
+});
+
+winston.info(`Application running in ${process.env.NODE_ENV || 'development'} mode.`);
+
+const app = express();
+
 const host = process.env.VMS_HOST || (prod ? '0.0.0.0' : '127.0.0.1');
 const port = process.env.VMS_PORT || (prod ? 80 : 8080);
-console.log(`API will be listening on ${host}:${port}`);
 
-app.listen(+port, host);
+app.listen(+port, host, () => {
+  winston.info(`HTTP server listening on http://${host}:${port}.`);
+});
